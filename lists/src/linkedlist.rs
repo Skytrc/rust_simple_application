b5105f47fd195b7e08db095777a2818fee64ad65
kwongfung <skytrc@163.com>
2023-07-06 00:54:03 +0800

    feat: deque push_front and pop_front

--- lists/src/linkedlist.rs
+++ lists/src/linkedlist.rs
@@ -1,4 +1,4 @@
-use std::ptr::NonNull;
+use std::{ptr::NonNull, marker::PhantomData};
 
 pub struct LinkedList<T> {
     front: Link<T>,
@@ -25,4 +25,57 @@ impl<T> LinkedList<T> {
             _boo: PhantomData,
         }
     }
+
+    pub fn push_front(&mut self, elem: T) {
+        unsafe {
+            // 利用Box智能分配Node在堆上的空间
+            let new = NonNull::new_unchecked(Box::into_raw(Box::new(Node {
+                front: None,
+                back: None,
+                elem,
+            })));
+            // 如果链表不为空，重新设置新旧链表头的关系
+            if let Some(old) = self.front {
+                (*old.as_ptr()).front = Some(new);
+                (*new.as_ptr()).back = Some(old);
+            } else {
+                // 如果链表为空，链表尾也为新的node
+                debug_assert!(self.back.is_none());
+                debug_assert!(self.front.is_none());
+                debug_assert!(self.len == 0);
+                self.back = Some(new);
+            }
+            // 设置新的链表头
+            self.front = Some(new);
+            self.len += 1;
+        }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        unsafe {
+            self.front.map(|node| {
+                // 利用Box在不需要的时候自动释放
+                let boxed_node = Box::from_raw(node.as_ptr());
+                let result = boxed_node.elem;
+
+                // 重新设置链表头
+                self.front = boxed_node.back;
+                if let Some(new) = self.front {
+                    (*new.as_ptr()).front = None;
+                } else {
+                    // 如果链表中没有其他node，链表尾也为None
+                    debug_assert!(self.len == 1);
+                    self.back = None;
+                }
+
+                self.len -= 1;
+                result
+            })
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
 }
+
